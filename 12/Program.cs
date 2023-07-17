7c64c77f65193c7ab6d36d74d9d9c291538b1dff
azadei <azadei@code.edu.az>
2023-07-17 15:00:01 +0400

    1

---
+++ 12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Input the string: ");
+        string inputString = Console.ReadLine();
+
+        
+        string[] words = inputString.Split(' ');
+
+        
+        var upperCaseWords = words.Where(word => word == word.ToUpper());
+
+        
+        Console.WriteLine("The UPPER CASE words are:");
+        foreach (string word in upperCaseWords)
+        {
+            Console.WriteLine(word);
+        }
+
+       
+    }
+}
